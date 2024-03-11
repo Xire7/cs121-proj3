15ae6a709ad63178e5111dcfd201aecf7dca508a
Steven-Tu <steventu102@gmail.com>
2024-03-10 23:00:50 -0700

    final working

--- frontend/init.py
+++ frontend/init.py
@@ -12,22 +12,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     print("home")
-    return render_template("index.html", content=True)
+    return render_template("index.html")
 
 @app.route('/result/<results>')
 def results(results=None):
-    print("main POOP")
     if results:
         urls = main.main(results)
         if len(urls) > 10:
              pages = [urls [i:i + 6] for i in range(0, len(urls), 6) ]
-        if len(urls) == 0:
+        elif len(urls) == 0:
             return render_template("index.html")
         else:
             pages = [urls]
         return render_template("results.html",pages=pages, query=results, length=len(pages))
-    #empty or not found page later
-    print("emtty")
     
     
 @app.route('/result')
